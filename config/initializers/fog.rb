8ec15c2e3908cb97e58e084637555ad0de64a34f
Sungguk Cho <sung9gi@gmail.com>
2018-04-17 06:56:48 +0000

    file upload with s3

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -2,9 +2,9 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'  # required
   config.fog_credentials = {
     provider:              'AWS',  # required
-    aws_access_key_id:     'AKIAIBMSDIB24KITHFXA',  # required
-    aws_secret_access_key: '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',  # required
+    aws_access_key_id:     'AKIAIBMSDIB24KITHFXA',  # SG_S3 
+    aws_secret_access_key: '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',  # SG_S3
     region:                'ap-northeast-2',  
   }
-  config.fog_directory  = 'moqa-blog'            # required
+  config.fog_directory  = 'moqa-blog'            # S3 bucket name
 end
